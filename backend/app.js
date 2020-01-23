64588db542e1d09ad8278d6bd5c3da874f7acf2f
antoinehng <henning.antoine@gmail.com>
2020-01-23 18:12:03 +0100

    use socket to carry user_id

--- backend/app.js
+++ backend/app.js
@@ -3,20 +3,17 @@ const app = require('express')()
 const http = require('http').createServer(app)
 const io = require('socket.io')(http)
 
-let users = {}
-
 io.on('connection', function(socket){
-    user_id = uuid()
-    users[user_id] = socket
-    console.log('User connected: ', user_id)
+    socket.user_id = uuid()
+    console.log('User connected: ', socket.user_id)
 
     socket.on('new_game', function(msg, callback){
         console.log('> new_game: ', msg)
         new_game_id = uuid()
         socket.join(new_game_id)
 
-        response = {"user_id": user_id, "game_id": new_game_id}
-        console.log('< new_game: ', {"user_id": user_id, game_id: new_game_id})
+        response = {user_id: socket.user_id, game_id: new_game_id}
+        console.log('< new_game: ', response)
         callback(response)
     });
 
@@ -25,17 +22,16 @@ io.on('connection', function(socket){
         game_id = msg.game_id
         socket.join(game_id)
 
-        io.to(game_id).emit('new_player', user_id)
+        io.to(game_id).emit('new_player', socket.user_id)
 
-        response = {"user_id": user_id, "game_id": game_id}
-        console.log('< join_game: ', {"user_id": user_id, "game_id": game_id})
+        response = {user_id: socket.user_id, game_id: game_id}
+        console.log('< join_game: ', response)
         callback(response)
     });
 
 
     socket.on('disconnect', function () {
-        delete users[user_id]
-        console.log('User disconnected: ', user_id)
+        console.log('User disconnected: ', socket.user_id)
     });
 
   });
