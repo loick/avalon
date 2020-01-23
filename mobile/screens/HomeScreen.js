d57bf5fc591fea710cde7a077e547478242a1df2
Loick Le Digabel <loick.ledigabel@gmail.com>
2020-01-23 18:00:15 +0100

    Make the create room work

--- mobile/screens/HomeScreen.js
+++ mobile/screens/HomeScreen.js
@@ -1,16 +1,40 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Button, StyleSheet, View } from 'react-native'
-import RoomId from '../components/RoomId'
+// import RoomId from '../components/RoomId'
+import { createGame } from '../socket'
 
-const BUTTONS = ['CREATE', 'JOIN']
+const BUTTONS = [{ name: 'JOIN', path: '' }]
+
+const createRoomButton = () => {
+  const [clicked, setCliked] = useState(null)
+
+  useEffect(() => {
+    const joinRoom = async () => {
+      const { game_id, user_id } = await createGame()
+    }
+
+    if (clicked) {
+      joinRoom()
+    }
+  }, [clicked])
+
+  return <Button title="Create" onPress={() => setCliked(true)} />
+}
 
 export default function HomeScreen() {
   return (
     <View style={styles.container}>
-      <RoomId />
+      {/* <RoomId /> */}
       {BUTTONS.map(button => (
-        <Button key={button} title={button} />
+        <Button
+          key={button.name}
+          title={button.name}
+          onPress={() => {
+            alert('You tapped the button!')
+          }}
+        />
       ))}
+      {createRoomButton()}
     </View>
   )
 }
