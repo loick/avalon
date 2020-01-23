e2474b44568bb5a9860d1299bc162a49a7e87986
antoinehng <henning.antoine@gmail.com>
2020-01-23 18:01:54 +0100

    clean

--- backend/app.js
+++ backend/app.js
@@ -8,12 +8,13 @@ let users = {}
 io.on('connection', function(socket){
     user_id = uuid()
     users[user_id] = socket
-    console.log('New user connected: ', user_id)
+    console.log('User connected: ', user_id)
 
     socket.on('new_game', function(msg, callback){
         console.log('> new_game: ', msg)
         new_game_id = uuid()
         socket.join(new_game_id)
+
         response = {"user_id": user_id, "game_id": new_game_id}
         console.log('< new_game: ', {"user_id": user_id, game_id: new_game_id})
         callback(response)
@@ -23,7 +24,9 @@ io.on('connection', function(socket){
         console.log('> join_game: ', msg)
         game_id = msg.game_id
         socket.join(game_id)
-        io.to(game_id).emit('new_player', user_id);
+
+        io.to(game_id).emit('new_player', user_id)
+
         response = {"user_id": user_id, "game_id": game_id}
         console.log('< join_game: ', {"user_id": user_id, "game_id": game_id})
         callback(response)
@@ -32,6 +35,7 @@ io.on('connection', function(socket){
 
     socket.on('disconnect', function () {
         delete users[user_id]
+        console.log('User disconnected: ', user_id)
     });
 
   });
