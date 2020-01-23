33285c0dda3f861643691db6cf6dd8fe03cace8a
ThibautM <thibaut.marsal@gmail.com>
2020-01-23 21:56:23 +0100

    add style on join screen

---
+++ mobile/components/Choice.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import { Button } from 'react-native'
+
+export default function Choice({ title }) {
+  return <Button title={title} />
+}
--- mobile/screens/Join.js
+++ mobile/screens/Join.js
@@ -1,13 +1,29 @@
 import React, { useState } from 'react'
-import { Button, View, TextInput } from 'react-native'
+import { Button, View, TextInput, StyleSheet } from 'react-native'
 
 export default function Join({ navigation: { navigate } }) {
   const [value, onChangeText] = useState('Useless Placeholder')
 
   return (
-    <View>
-      <TextInput value={value} onChangeText={text => onChangeText(text)} />
+    <View style={styles.container}>
+      <TextInput
+        style={styles.input}
+        value={value}
+        onChangeText={text => onChangeText(text)}
+      />
       <Button title="Join" />
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  input: {
+    borderWidth: 0.5,
+    borderColor: '#d6d7da',
+  },
+})
