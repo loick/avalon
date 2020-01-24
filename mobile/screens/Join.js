a6681f594800dcc2ce3c1b1cb97e7c8ecf80df80
Loick Le Digabel <loick.ledigabel@gmail.com>
2020-01-24 14:43:10 +0100

    Pimp the join page with loading & error state

--- mobile/screens/Join.js
+++ mobile/screens/Join.js
@@ -1,30 +1,48 @@
 import React, { useState, useEffect } from 'react'
 import { View, StyleSheet } from 'react-native'
-import { Button, TextInput } from 'react-native-paper'
+import { Text, Button, TextInput, ActivityIndicator } from 'react-native-paper'
 import { joinGame } from '../socket'
 
+const JOINING_STATE = {
+  INIT: '',
+  INPUT: 'JOINING_STATE_INPUT',
+  WAITING_GAME: 'JOINING_WAITING_GAME',
+  ERROR: 'JOINING_ERROR',
+}
+
 export default function Join({ navigation: { navigate } }) {
-  const [valid, onValid] = useState(false)
+  const [pageState, onPageStateChange] = useState(JOINING_STATE.INIT)
   const [value, onChangeText] = useState('')
 
   useEffect(() => {
+    let joining = false
     const joinRoom = async () => {
-      const { game_invite_code, error } = await joinGame(value)
+      const { game, error } = await joinGame(value)
       if (error) {
-        onValid(false)
-        console.log(error)
+        joining = false
+        onPageStateChange(JOINING_STATE.ERROR)
       }
 
-      if (game_invite_code) {
-        console.log('ready, go to loading screen')
+      if (game.invite_code) {
+        onPageStateChange(JOINING_STATE.WAITING_GAME)
       }
     }
-    if (value && !valid) {
+
+    if (!joining && pageState === JOINING_STATE.INPUT) {
+      joining = true
       joinRoom()
-    } else {
-      onValid(false)
     }
-  }, [valid])
+  }, [pageState])
+
+  const onInputChange = value => {
+    onPageStateChange(JOINING_STATE.INIT)
+    onChangeText(value.toUpperCase())
+  }
+
+  const isJoiningTheRoom = pageState === JOINING_STATE.INPUT
+  const isError = pageState === JOINING_STATE.ERROR
+  const isJoiningWaitingForGameToSTart =
+    pageState === JOINING_STATE.WAITING_GAME
 
   return (
     <View style={styles.container}>
@@ -33,11 +51,31 @@ export default function Join({ navigation: { navigate } }) {
         label="Room id"
         value={value}
         maxLength={4}
-        onChangeText={v => onChangeText(v.toUpperCase())}
+        onChangeText={onInputChange}
       />
-      <Button mode="outlined" onPress={() => onValid(true)}>
+      <Button
+        mode="outlined"
+        onPress={() => onPageStateChange(JOINING_STATE.INPUT)}
+      >
         Join
       </Button>
+      {isJoiningTheRoom && (
+        <View>
+          <Text>Joining the room...</Text>
+          <ActivityIndicator activity />
+        </View>
+      )}
+      {isJoiningWaitingForGameToSTart && (
+        <View>
+          <Text>Waiting for the creator to start the game...</Text>
+          <ActivityIndicator activity />
+        </View>
+      )}
+      {isError && (
+        <View>
+          <Text>This room does not exist</Text>
+        </View>
+      )}
     </View>
   )
 }
